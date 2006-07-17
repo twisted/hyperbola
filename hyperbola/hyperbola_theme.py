a66830c3735c86d0119deeefbf8833b11973ae1a
exarkun <>
2006-07-17 15:57:27 +0000

    Revert r7794 - Nevow/Python 2.3 regression

--- hyperbola/hyperbola_theme.py
+++ hyperbola/hyperbola_theme.py
@@ -2,12 +2,9 @@ from xmantissa import webtheme
 from nevow import tags
 
 class HyperbolaTheme(webtheme.XHTMLDirectoryTheme):
-    def head(self, website):
-        root = website.cleartextRoot()
-        yield tags.link(
-            href=root + '/Hyperbola/static/hyperbola.css',
-            rel='stylesheet',
-            type='text/css')
-        yield tags.script(
-            src=root + '/Hyperbola/static/hyperbola.js',
-            type='text/javascript')
+    def head(self):
+        yield tags.link(href='/static/hyperbola/hyperbola.css',
+                        rel='stylesheet', type='text/css')
+        yield tags.script(src='/static/hyperbola/hyperbola.js',
+                          type='text/javascript')
+
